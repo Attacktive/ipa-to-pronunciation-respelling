0805308fc5ad70ffe6c9b9c859bdf0f761e790ff
Attacktive <tribute2pantera@gmail.com>
2023-10-04 18:39:07 +0900

    feature: add temporary initial implementation

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [![CodeFactor](https://www.codefactor.io/repository/github/attacktive/ipa-to-pronunciation-respelling/badge)](https://www.codefactor.io/repository/github/attacktive/ipa-to-pronunciation-respelling)
 
-Respells [IPA (International Phonetic Alphabet)](https://en.wikipedia.org/wiki/International_Phonetic_Alphabet) based on [this article](https://en.wikipedia.org/wiki/Help:Pronunciation_respelling_key).
+Respells [IPA (International Phonetic Alphabet)](https://en.wikipedia.org/wiki/International_Phonetic_Alphabet) based on [this article](https://en.wikipedia.org/wiki/Help:Pronunciation_respelling_key) and [this](https://www.dictionary.com/e/key-to-ipa-pronunciations/).
---
+++ index.js
@@ -0,0 +1,68 @@
+function loaded() {
+	focusOnInput();
+}
+
+function focusOnInput() {
+	const input = document.querySelector("#input");
+	input.select();
+}
+
+function onInput(event) {
+	const input = document.querySelector("#input");
+	const button = document.querySelector("#run");
+
+	if (input.value.length > 0) {
+		button.removeAttribute("disabled");
+	} else {
+		button.setAttribute("disabled", "");
+	}
+}
+
+/**
+ * @param { KeyboardEvent } event
+ */
+function preventFormSubmission(event) {
+	if (event.key === "Enter") {
+		return false;
+	}
+}
+
+/**
+ * @param { KeyboardEvent } event
+ */
+function onKeydown(event) {
+	if (event.key === "Enter") {
+		run();
+	}
+}
+
+function run() {
+	const input = document.querySelector("#input");
+	const output = document.querySelector("#output");
+
+	let result;
+
+	try {
+		result = convert(input.value);
+	} catch (error) {
+		console.error(error);
+
+		result = error.message;
+	}
+
+	output.textContent = result;
+}
+
+addEventListener("load", loaded);
+
+const forms = document.querySelectorAll("form");
+for (const form of Array.from(forms)) {
+	form.onkeydown = preventFormSubmission;
+}
+
+const input = document.querySelector("#input");
+input.addEventListener("input", onInput);
+input.addEventListener("keydown", onKeydown);
+
+const button = document.querySelector("#run");
+button.addEventListener("click", run);
