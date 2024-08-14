61f6bd269e0c9d5aad5fcde72bc6967c483e862f
Attacktive <tribute2pantera@gmail.com>
2024-08-14 14:51:09 +0900

    refactor: replace for...of loops by `map` and `forEach` combination
    ...because it's cool. 🙄

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -26,26 +26,28 @@ function drawInputButtons() {
 	};
 
 	const consonantInputButtonArea = document.querySelector("#input-button-area-consonants") as HTMLDivElement;
-	for (const consonant of [...consonants, STRESS_MARK]) {
+	[...consonants, STRESS_MARK].map(consonant => {
 		const button = document.createElement("button");
 		button.type = "button";
 		button.innerText = consonant;
 		button.className = "col btn btn-secondary input-button";
 		button.onclick = onButtonClick;
 
-		consonantInputButtonArea.appendChild(button);
-	}
+		return button;
+	})
+	.forEach(button => consonantInputButtonArea.appendChild(button));
 
 	const vowelInputButtonArea = document.querySelector("#input-button-area-vowels") as HTMLDivElement;
-	for (const vowel of vowels) {
+	vowels.map(vowel => {
 		const button = document.createElement("button");
 		button.type = "button";
 		button.innerText = vowel;
 		button.className = "col btn btn-secondary input-button";
 		button.onclick = onButtonClick;
 
-		vowelInputButtonArea.appendChild(button);
-	}
+		return button;
+	})
+	.forEach(button => vowelInputButtonArea.appendChild(button));
 }
 
 function onInput() {
@@ -92,12 +94,12 @@ function copy() {
 	const output = document.querySelector("#output") as HTMLElement;
 
 	navigator.clipboard.writeText(output.textContent as string)
-		.then(() => {
-			const message = `"${output.textContent}" is successfully copied to the clipboard. 😁`;
+	.then(() => {
+		const message = `"${output.textContent}" is successfully copied to the clipboard. 😁`;
 
-			console.debug(message);
-			showToast(message);
-		});
+		console.debug(message);
+		showToast(message);
+	});
 }
 
 function showToast(message: string) {
