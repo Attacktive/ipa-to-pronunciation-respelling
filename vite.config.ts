eaf9c1ff9972f0e347aebc3c8b6e0fc2626199db
Attacktive <tribute2pantera@gmail.com>
2025-07-27 20:42:13 +0900

    fix: copy favicon.ico

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite';
+import { copyFileSync, mkdirSync } from 'fs';
+import { join } from 'path';
 
 export default defineConfig({
 	root: '.',
@@ -8,5 +10,19 @@ export default defineConfig({
 	},
 	server: {
 		port: 8080
-	}
+	},
+	plugins: [
+		{
+			name: 'copy favicon',
+			closeBundle: () => {
+				const destDir = join(__dirname, 'dist');
+
+				mkdirSync(destDir, { recursive: true });
+				copyFileSync(
+					join(__dirname, 'src', 'static', 'favicon.ico'),
+					join(destDir, 'favicon.ico')
+				);
+			}
+		}
+	]
 });
